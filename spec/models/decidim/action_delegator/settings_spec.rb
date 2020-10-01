b2997236dcd2d05dcba45cb1a66e7e40f44560ed
Pau Perez <saulopefa@gmail.com>
2020-10-01 19:29:37 +0200

    Remove useless expires_at column
    Since a setting is tied to a consultation, all its delegations will
    become stale when the consultation is over removing the need for
    a `expires_at`. They'll need to create a new Setting and new Delegation
    records.

--- spec/models/decidim/action_delegator/settings_spec.rb
+++ spec/models/decidim/action_delegator/settings_spec.rb
@@ -11,18 +11,7 @@ module Decidim
       it { is_expected.to have_many(:delegations).dependent(:destroy) }
 
       it { is_expected.to validate_presence_of(:max_grants) }
-      it { is_expected.to validate_presence_of(:expires_at) }
       it { is_expected.to validate_numericality_of(:max_grants).is_greater_than(0) }
-
-      context "when the expires_at is in the past" do
-        subject { build(:setting, expires_at: Time.zone.now - 1.day) }
-
-        it { is_expected.not_to be_valid }
-      end
-
-      context "when the expires_at is in the future" do
-        it { is_expected.to be_valid }
-      end
     end
   end
 end
