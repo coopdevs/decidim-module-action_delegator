f8c9ff2967d14f650e07c86a1465e6e40b59abdb
Maxim Colls <collsmaxim@gmail.com>
2020-10-06 12:11:10 +0200

    Fix table name

--- db/migrate/20201006084522_remove_setting_id_index_from_delegations.rb
+++ db/migrate/20201006084522_remove_setting_id_index_from_delegations.rb
@@ -2,6 +2,6 @@
 
 class RemoveSettingIdIndexFromDelegations < ActiveRecord::Migration[5.2]
   def change
-    remove_index :accounts, name: "index_decidim_delegations_on_action_delegator_setting_id"
+    remove_index :decidim_action_delegator_delegations, name: "index_decidim_delegations_on_action_delegator_setting_id"
   end
 end
