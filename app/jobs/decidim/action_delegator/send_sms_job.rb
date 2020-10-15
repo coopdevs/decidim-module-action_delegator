f5645f8618d4e1c2604bd52b6287cb557c04210a
Maxim Colls <collsmaxim@gmail.com>
2020-10-15 16:29:44 +0200

    Added dumb job to test sending sms from worker

---
+++ app/jobs/decidim/action_delegator/send_sms_job.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+require "savon"
+
+module Decidim
+  module ActionDelegator
+    class SendSmsJob < ApplicationJob
+      queue_as :default
+
+      def perform(mobile_phone_number)
+        client = ::Savon.client(wsdl: "https://websms.smsvirtual.es/api_php/smsvirtual.wsdl")
+
+        response = client.call(:send_sms,
+                               message: {
+                                 user: ENV["SMS_USER"],
+                                 pass: ENV["SMS_PASS"],
+                                 src: "Coopcat",
+                                 dst: mobile_phone_number,
+                                 msg: "Test"
+                               })
+
+        Rails.logger.debug response
+      end
+    end
+  end
+end
