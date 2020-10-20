4a45e64b173f95714dfe3c18753aaa47f9653d7a
Maxim Colls <collsmaxim@gmail.com>
2020-10-20 12:09:52 +0200

    Moved soap request into a separate Job

--- app/jobs/decidim/action_delegator/send_sms_job.rb
+++ app/jobs/decidim/action_delegator/send_sms_job.rb
@@ -4,22 +4,58 @@ require "savon"
 
 module Decidim
   module ActionDelegator
+    class SendSmsJobException < StandardError; end
     class SendSmsJob < ApplicationJob
       queue_as :default
 
-      def perform(mobile_phone_number)
-        client = ::Savon.client(wsdl: "https://websms.masmovil.com/api_php/smsvirtual.wsdl")
+      attr_reader :response
 
-        response = client.call(:send_sms,
-                               message: {
-                                 user: ENV["SMS_USER"],
-                                 pass: ENV["SMS_PASS"],
-                                 src: "Coopcat",
-                                 dst: mobile_phone_number,
-                                 msg: "Test"
-                               })
+      SMSVIRTUAL_WSDL_URL = "https://websms.masmovil.com/api_php/smsvirtual.wsdl"
 
-        Rails.logger.debug response
+      def perform(sender_name, mobile_phone_number, message)
+        @sender_name = sender_name
+        @mobile_phone_number = mobile_phone_number
+        @message = message
+
+        send_sms!
+
+        raise SendSmsJobException, @response unless success?
+      end
+
+      private
+
+      attr_reader :sender_name, :mobile_phone_number, :message
+
+      def send_sms!
+        @response = client.call(:send_sms,
+                                message: {
+                                  user: ENV["SMS_USER"],
+                                  pass: ENV["SMS_PASS"],
+                                  src: sender_name,
+                                  dst: mobile_phone_number,
+                                  msg: message
+                                })
+      end
+
+      def success?
+        parsed_response[:code] == "200"
+      end
+
+      def client
+        @client ||= ::Savon.client(wsdl: SMSVIRTUAL_WSDL_URL)
+      end
+
+      def parsed_response
+        return @parsed_response if @parsed_response
+
+        doc = Nokogiri::XML response.body[:send_sms_response][:result]
+
+        @parsed_response = {
+          code: doc.xpath("//codigo").text,
+          description: doc.xpath("//descripcion").text
+        }
+
+        @parsed_response
       end
     end
   end
--- app/services/decidim/action_delegator/sms_gateway.rb
+++ app/services/decidim/action_delegator/sms_gateway.rb
@@ -11,51 +11,15 @@ module Decidim
       end
 
       def deliver_code
-        send_sms!
+        SendSmsJob.perform_later(sender_name, mobile_phone_number, message)
 
-        success?
+        true
       end
 
       private
 
-      def success?
-        parsed_response[:code] == "200"
-      end
-
-      def parsed_response
-        return @parsed_response if @parsed_response
-
-        doc = Nokogiri::XML response.body[:send_sms_response][:result]
-
-        @parsed_response = {
-          code: doc.xpath("//codigo").text,
-          description: doc.xpath("//descripcion").text
-        }
-
-        @parsed_response
-      end
-
-      def send_sms!
-        @response = client.call(:send_sms,
-                                message: {
-                                  user: ENV["SMS_USER"],
-                                  pass: ENV["SMS_PASS"],
-                                  src: sender_name,
-                                  dst: mobile_phone_number,
-                                  msg: message
-                                })
-
-        Rails.logger.debug "==========="
-        Rails.logger.debug ["action_delegator_gateway", "send_sms!", response].map(&:inspect).join("\n")
-        Rails.logger.debug "==========="
-      end
-
       def sender_name
         ENV["SMS_SENDER_NAME"] || "Decidim"
-      end
-
-      def client
-        @client ||= ::Savon.client(wsdl: "https://websms.masmovil.com/api_php/smsvirtual.wsdl")
       end
 
       def message
