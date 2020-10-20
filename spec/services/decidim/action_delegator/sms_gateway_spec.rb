8b499df36ca2b93180f0012fdf8b57750e6f8776
Maxim Colls <collsmaxim@gmail.com>
2020-10-20 13:19:32 +0200

    Fixed specs after moving code to a Job

--- spec/services/decidim/action_delegator/sms_gateway_spec.rb
+++ spec/services/decidim/action_delegator/sms_gateway_spec.rb
@@ -7,31 +7,11 @@ module Decidim::ActionDelegator
     let(:subject) { described_class.new(mobile_phone_number, code) }
     let(:mobile_phone_number) { "+12 345 678 901" }
     let(:code) { "1a4s9b" }
-    let(:client) { double("client") }
-    let(:response) { double("response") }
     let(:response_body) { { send_sms_response: { result: result } } }
 
     describe "#deliver_code" do
-      before do
-        allow(Savon).to receive(:client).and_return(client)
-        allow(client).to receive(:call).and_return(response)
-        allow(response).to receive(:body).and_return(response_body)
-      end
-
-      context "when code's result is 200" do
-        let(:result) { "<xml><codigo>200</codigo><descripcion>Went well</descripcion></xml>" }
-
-        it "returns true" do
-          expect(subject.deliver_code).to eq(true)
-        end
-      end
-
-      context "when code's result isn't 200" do
-        let(:result) { "<xml><codigo>115</codigo><descripcion>Woopsie</descripcion></xml>" }
-
-        it "returns false" do
-          expect(subject.deliver_code).to eq(false)
-        end
+      it "enqueues a SendSmsJob" do
+        expect { subject.deliver_code }.to have_enqueued_job(SendSmsJob)
       end
     end
   end
