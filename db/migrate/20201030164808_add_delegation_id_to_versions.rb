653ddaa035084e984f577ddd134e99474bc13aeb
Pau Perez <saulopefa@gmail.com>
2020-11-02 17:24:11 +0100

    Add query object to get versions of a consultation
    This is intended to be used to fetch all versions of votes performed
    through a delegation in case a court asks us to do so.

--- db/migrate/20201030164808_add_delegation_id_to_versions.rb
+++ db/migrate/20201030164808_add_delegation_id_to_versions.rb
@@ -3,5 +3,6 @@
 class AddDelegationIdToVersions < ActiveRecord::Migration[5.2]
   def change
     add_column :versions, :decidim_action_delegator_delegation_id, :integer, null: true, default: nil
+    add_index :versions, :decidim_action_delegator_delegation_id
   end
 end
---
+++ spec/queries/decidim/action_delegator/delegated_votes_versions_spec.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+module Decidim::ActionDelegator
+  describe DelegatedVotesVersions do
+    subject { described_class.new(consultation) }
+
+    let(:organization) { create :organization }
+    let(:user) { create(:user, :confirmed, organization: organization) }
+
+    let(:consultation) { create(:consultation, organization: organization) }
+    let(:question) { create(:question, consultation: consultation) }
+    let(:setting) { create(:setting, consultation: consultation) }
+    let(:delegation) { create(:delegation, setting: setting, granter: user) }
+    let!(:vote) { create(:vote, author: delegation.granter, question: question) }
+
+    let!(:other_consultation) { create(:consultation, organization: organization) }
+    let(:other_question) { create(:question, consultation: other_consultation) }
+    let(:other_setting) { create(:setting, consultation: other_consultation) }
+    let!(:other_delegation) { create(:delegation, setting: other_setting, granter: user) }
+    let!(:other_vote) { create(:vote, author: other_delegation.granter, question: other_question) }
+
+    describe "#query", versioning: true do
+      it "enables fetching all versions related to a consultation" do
+        result = subject.query
+        version = vote.versions.last
+
+        expect(result.size).to eq(1)
+        expect(result.first).to include(version.attributes.slice(:id))
+      end
+    end
+  end
+end
