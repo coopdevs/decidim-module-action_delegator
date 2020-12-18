ad50b993d062f6447e18cfc801a5b35e3b782c87
Laura Jaime <laura.jv@coditramuntana.com>
2020-12-18 09:02:28 +0100

    Remove Deface and use duplicate view
     * Remove Deface gem
     * Remove override views
     * Use duplicate view results.html.erb

--- app/overrides/views/decidim/consultations/admin/consultations/add_delegate_votes_by_results_view.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-Deface::Override.new(virtual_path: "decidim/consultations/admin/consultations/results",
-                     name: "add_delegate_votes_by_results_view",
-                     insert_after: "erb[loud]:contains('count: question.total_votes')",
-                     text: " / <%= t 'decidim.admin.consultations.results.total_delegates', count: question.total_delegates %>")
