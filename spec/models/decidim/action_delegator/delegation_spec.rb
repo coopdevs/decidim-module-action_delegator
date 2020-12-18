7fa644eb11a87bc25b8d5ae07307260eb90f584e
Laura Jaime <laura.jv@coditramuntana.com>
2020-12-18 09:11:57 +0100

    Add specs
     * Spec for total_delegates in consultation.
     * Spec for new function .granter_to?

--- spec/models/decidim/action_delegator/delegation_spec.rb
+++ spec/models/decidim/action_delegator/delegation_spec.rb
@@ -10,23 +10,41 @@ module Decidim
       it { is_expected.to belong_to(:setting) }
       it { is_expected.to be_valid }
 
-      describe ".granted_to?" do
+      describe ".granted/granter_to?" do
         let(:user) { create(:user) }
         let(:organization) { create(:organization) }
         let(:consultation) { create(:consultation, :active, organization: organization) }
         let(:setting) { create(:setting, consultation: consultation) }
 
-        context "when there is no delegation granted to user for the given consultation" do
-          it "returns false" do
-            expect(described_class.granted_to?(user, consultation)).to eq(false)
+        describe ".granted_to?" do
+          context "when there is no delegation granted to user for the given consultation" do
+            it "returns false" do
+              expect(described_class.granted_to?(user, consultation)).to eq(false)
+            end
+          end
+
+          context "when there are delegations granted to user for the given consultation" do
+            let!(:delegation) { create(:delegation, setting: setting, grantee: user) }
+
+            it "returns true" do
+              expect(described_class.granted_to?(user, consultation)).to eq(true)
+            end
           end
         end
 
-        context "when there are delegations granted to user for the given consultation" do
-          let!(:delegation) { create(:delegation, setting: setting, grantee: user) }
+        describe ".granter_to?" do
+          context "when there is no delegation granter to user for the given consultation" do
+            it "returns false" do
+              expect(described_class.granter_to?(user, consultation)).to eq(false)
+            end
+          end
 
-          it "returns true" do
-            expect(described_class.granted_to?(user, consultation)).to eq(true)
+          context "when there are delegations granter to user for the given consultation" do
+            let!(:delegation) { create(:delegation, setting: setting, granter: user) }
+
+            it "returns true" do
+              expect(described_class.granter_to?(user, consultation)).to eq(true)
+            end
           end
         end
       end
