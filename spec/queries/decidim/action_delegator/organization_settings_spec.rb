8887f53cd1ea6db57e72f49faa23e25e8f778908
Pau Perez <saulopefa@gmail.com>
2021-02-05 17:08:16 +0100

    Extend test coverage of query object
    I was aiming to reproduce the nil that's breaking
    `Admin::DelegationsController#organization_settings` and
    `Admin::SettingsController` to break but I can't reproduce it. This
    successfully returns an empty AR Relation.

--- spec/queries/decidim/action_delegator/organization_settings_spec.rb
+++ spec/queries/decidim/action_delegator/organization_settings_spec.rb
@@ -2,18 +2,28 @@
 
 require "spec_helper"
 
-describe Decidim::ActionDelegator::OrganizationSettings do
-  subject { described_class.new(organization) }
+module Decidim::ActionDelegator
+  describe OrganizationSettings do
+    subject { described_class.new(organization) }
 
-  let(:organization) { create(:organization) }
-  let(:consultation) { create(:consultation, organization: organization) }
-  let!(:setting) { create(:setting, consultation: consultation) }
+    let(:organization) { create(:organization) }
+    let(:consultation) { create(:consultation, organization: organization) }
 
-  let!(:other_setting) { create(:setting) }
+    describe "#query" do
+      context "when the organization has a setting" do
+        let!(:setting) { create(:setting, consultation: consultation) }
+        let!(:other_setting) { create(:setting) }
 
-  describe "#query" do
-    it "returns settings of the specified organization only" do
-      expect(subject.query).to match_array([setting])
+        it "returns settings of the specified organization only" do
+          expect(subject.query).to match_array([setting])
+        end
+      end
+
+      context "when the organization has no settings" do
+        it "returns an empty ActiveRecord::Relation" do
+          expect(subject.query).to be_empty
+        end
+      end
     end
   end
 end
