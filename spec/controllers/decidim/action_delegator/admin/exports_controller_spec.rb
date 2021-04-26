69bb04a41bfb2568c083b4f4ed5eee72419582b6
Pau Perez <saulopefa@gmail.com>
2021-04-26 16:50:25 +0200

    Test ExportJob is called

--- spec/controllers/decidim/action_delegator/admin/exports_controller_spec.rb
+++ spec/controllers/decidim/action_delegator/admin/exports_controller_spec.rb
@@ -9,7 +9,7 @@ module Decidim
 
       let(:organization) { create(:organization) }
       let(:user) { create(:user, :admin, :confirmed, organization: organization) }
-      let(:consultation) { create(:consultation, :finished, :unpublished_results, organization: organization) }
+      let(:consultation) { create(:consultation, :finished, :published_results, organization: organization) }
 
       before do
         request.env["decidim.current_organization"] = organization
@@ -18,8 +18,14 @@ module Decidim
 
       describe "#create" do
         it "authorizes the action" do
-          expect(controller).to receive(:allowed_to?).with(:export_consultation_results, :consultation, consultation: consultation)
+          expect(controller).to receive(:allowed_to?)
+            .with(:export_consultation_results, :consultation, consultation: consultation)
 
+          post :create, params: { consultation_slug: consultation.slug }
+        end
+
+        it "enqueues a ExportConsultationResultsJob" do
+          expect(ExportConsultationResultsJob).to receive(:perform_later).with(user, consultation)
           post :create, params: { consultation_slug: consultation.slug }
         end
       end
