502a6367d3c24f81bd1751556616e0b755c4dcc9
Pau Perez <saulopefa@gmail.com>
2021-06-08 11:48:11 +0200

    Remove redundant specs

--- spec/controllers/decidim/action_delegator/admin/consultations_controller_spec.rb
+++ spec/controllers/decidim/action_delegator/admin/consultations_controller_spec.rb
@@ -41,42 +41,9 @@ module Decidim
         context "when the consultation is finished" do
           let(:consultation) { create(:consultation, :finished, organization: organization) }
 
-          let(:other_question) { create(:question, consultation: consultation) }
-          let(:other_response) { create(:response, question: question) }
-
-          let(:granter) { create(:user, organization: organization) }
-          let(:grantee) { create(:user, organization: organization) }
-          let(:setting) { create(:setting, consultation: consultation) }
-
-          context "when there are no delegations" do
-            before do
-              other_question.votes.create(author: user, response: other_response)
-
-              puts "granter_id = #{granter.id}"
-              puts "other_question = #{other_question.id}"
-
-              create(:delegation, granter_id: granter.id, grantee_id: grantee.id, setting: setting)
-              other_question.votes.create(author: granter, response: other_response)
-            end
-
-            it "loads the question's totals without an N+1" do
-              get :results, params: { slug: consultation.slug }
-              questions = assigns(:questions)
-
-              expect(questions.first.id).to eq(question.id)
-              expect(questions.second.id).to eq(other_question.id)
-
-              expect(questions.first.total_delegates).to eq(0)
-              expect(questions.second.total_delegates).to eq(1)
-
-              expect(questions.first.total_participants).to eq(1)
-              expect(questions.second.total_participants).to eq(2)
-            end
-
-            it "loads the responses" do
-              get :results, params: { slug: consultation.slug }
-              expect(assigns(:responses)).not_to be_empty
-            end
+          it "loads the responses" do
+            get :results, params: { slug: consultation.slug }
+            expect(assigns(:responses)).not_to be_empty
           end
         end
       end
