1a0617b8c5c943fb931aa742a8c95accc31b80f8
Aitor Lopez Beltran <aitorlopezbeltran@gmail.com>
2021-07-21 17:49:59 +0200

    Change MAX_DECIDIM_VERSION to support decidim 0.24

--- lib/decidim/action_delegator/version.rb
+++ lib/decidim/action_delegator/version.rb
@@ -4,7 +4,7 @@ module Decidim
   # This holds the decidim-action_delegator version.
   module ActionDelegator
     MIN_DECIDIM_VERSION = ">= 0.22"
-    MAX_DECIDIM_VERSION = "< 0.24"
+    MAX_DECIDIM_VERSION = "<= 0.24"
     VERSION = "0.2"
   end
 end
