8a2e93c1068e0759622824b10a10a92e63e97b5d
LauraJaimeC <75725233+LauraJaimeC@users.noreply.github.com>
2022-11-07 10:26:07 +0100

    Upgrade to Decidim 0.26 (#118)
    * WIP: upgrade to Decidim 0.25
    
    * Execute bundle install
    
    * Remove unused image
    
    * Rubocopify
    
    * Take into account updated files for the next upgrade.
    
    * TEST: Solve helper resolution in the test env
    
    This solves problems of the kind: `undefined method `icon' for #<#<Class:0x00007fa518547c38>:0x00007fa518b9d510>`
    
    * Pending translations
    
    * TEST: make test succeed
    
    It is strange that they were working before without an organization with available_authorizations.
    
    * TEST: update test prerequisites
    
    * TEST: make test succeed
    
    * Upgrade Decidim to 0.26
    
    * Add Doorkeeper initializer
    
    * Fix tests
    
    Co-authored-by: Oliver Valls <199462+tramuntanal@users.noreply.github.com>

--- lib/decidim/action_delegator/version.rb
+++ lib/decidim/action_delegator/version.rb
@@ -3,8 +3,8 @@
 module Decidim
   # This holds the decidim-action_delegator version.
   module ActionDelegator
-    MIN_DECIDIM_VERSION = ">= 0.25.0"
-    MAX_DECIDIM_VERSION = "< 0.26.0"
-    VERSION = "0.5.0"
+    MIN_DECIDIM_VERSION = ">= 0.26.0"
+    MAX_DECIDIM_VERSION = "< 0.27.0"
+    VERSION = "0.6.0"
   end
 end
