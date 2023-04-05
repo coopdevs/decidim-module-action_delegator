d9a1757d94184b52e0c44e8c0b205270ad1718ea
Anna Topalidi <t.opalidisanna@gmail.com>
2023-04-05 15:27:09 +0200

    add rspec for ImportParticipantsCsvJob

---
+++ spec/jobs/decidim/action_delegator/admin/import_participants_csv_job_spec.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+RSpec.describe Decidim::ActionDelegator::Admin::ImportParticipantsCsvJob, type: :job do
+  let(:current_user) { create(:user) }
+  let(:valid_csv_file) { File.open("spec/fixtures/valid_participants.csv") }
+  let(:invalid_csv_file) { File.open("spec/fixtures/invalid_participants.csv") }
+  let(:current_setting) { create(:setting, consultation: consultation) }
+  let(:consultation) { create(:consultation) }
+  let(:importer) { instance_double("Decidim::ActionDelegator::ParticipantsCsvImporter") }
+  let(:import_summary) { instance_double("Decidim::ActionDelegator::ImportSummary") }
+
+  before do
+    allow(Decidim::ActionDelegator::ParticipantsCsvImporter).to receive(:new).with(valid_csv_file, current_user, current_setting).and_return(importer)
+    allow(importer).to receive(:import!).and_return(import_summary)
+  end
+
+  it "imports participants CSV file and sends email notification" do
+    expect(importer).to receive(:import!)
+    expect(Decidim::ActionDelegator::ImportParticipantsMailer).to receive(:import).with(current_user, import_summary).and_return(double("mailer", deliver_now: true))
+    expect { described_class.perform_now(current_user, valid_csv_file, current_setting) }.not_to raise_error
+  end
+end
