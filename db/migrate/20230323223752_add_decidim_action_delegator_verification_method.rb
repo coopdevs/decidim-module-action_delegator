033e79738e8b5feaff26cfc12d10dd6e97db0332
Ivan Vergés <ivan@pokecode.net>
2023-04-12 15:33:21 +0200

    user id relation (#140)
    * add user relation
    
    * cops

--- db/migrate/20230323223752_add_decidim_action_delegator_verification_method.rb
+++ db/migrate/20230323223752_add_decidim_action_delegator_verification_method.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddDecidimActionDelegatorVerificationMethod < ActiveRecord::Migration[6.0]
   def change
     add_column :decidim_action_delegator_settings, :authorization_method, :integer, default: 0, null: false
