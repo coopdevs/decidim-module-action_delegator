ed97b8905068998da397be551098cf79fa65a64b
Ivan Vergés <ivan@pokecode.net>
2023-05-16 11:12:02 +0200

    improve message on errors imported

--- spec/forms/decidim/action_delegator/admin/delegation_form_spec.rb
+++ spec/forms/decidim/action_delegator/admin/delegation_form_spec.rb
@@ -8,22 +8,56 @@ describe Decidim::ActionDelegator::Admin::DelegationForm do
   let(:organization) { create(:organization) }
   let(:granter) { create(:user, organization: organization) }
   let(:grantee) { create(:user, organization: organization) }
+  let(:granter_email) { nil }
+  let(:grantee_email) { nil }
+  let(:attributes) do
+    {
+      granter_id: granter&.id,
+      grantee_id: grantee&.id,
+      granter_email: granter_email,
+      grantee_email: grantee_email
+    }
+  end
+  let!(:granter_user) { create(:user, organization: organization) }
+  let!(:grantee_user) { create(:user, organization: organization) }
 
   context "when there's granter and grantee" do
-    let(:attributes) { { granter_id: granter.id, grantee_id: grantee.id } }
-
     it { is_expected.to be_valid }
   end
 
   context "when granter is missing" do
-    let(:attributes) { { granter_id: nil, grantee_id: grantee.id } }
+    let(:granter) { nil }
 
     it { is_expected.not_to be_valid }
+
+    context "and granter_email is present" do
+      let(:granter_email) { granter_user.email }
+
+      it { is_expected.to be_valid }
+
+      context "and granter is not registered" do
+        let(:granter_email) { "test@idontexist.com" }
+
+        it { is_expected.not_to be_valid }
+      end
+    end
   end
 
   context "when grantee is missing" do
-    let(:attributes) { { granter_id: granter.id, grantee_id: nil } }
+    let(:grantee) { nil }
 
     it { is_expected.not_to be_valid }
+
+    context "and grantee_email is present" do
+      let(:grantee_email) { grantee_user.email }
+
+      it { is_expected.to be_valid }
+
+      context "and grantee is not registered" do
+        let(:grantee_email) { "test@idontexist.com" }
+
+        it { is_expected.not_to be_valid }
+      end
+    end
   end
 end
