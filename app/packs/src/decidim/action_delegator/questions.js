16279352eef441df108fd50d8de7ca7d7cca61a5
Ivan Vergés <ivan@pokecode.net>
2024-02-12 15:54:22 +0100

    fix ajax delegations voting

--- app/packs/src/decidim/action_delegator/questions.js
+++ app/packs/src/decidim/action_delegator/questions.js
@@ -1,13 +1,14 @@
 $(() => {
   const 
-      delegationCallouts = $(".delegation-callout"),
-      delegationCalloutsMessage = $(".delegation-callout-message"),
-      delegationDialog = $("#delegations-modal"),
-      delegationField = $("#decidim_consultations_delegation_id"),
-      delegationVoteButtons = $(".delegation-vote-button"),
-      delegationsButton = $("#delegations-button"),
-      voteButton = $("#vote_button"),
-      voteDialog = $("#question-vote-modal");
+    delegationCallouts = $(".delegation-callout"),
+    delegationCalloutsMessage = $(".delegation-callout-message"),
+    delegationDialog = $("#delegations-modal"),
+    delegationField = $("#decidim_consultations_delegation_id"),
+    delegationVoteButtons = $(".delegation-vote-button"),
+    delegationUnVoteButtons = $(".delegation_unvote_button"),
+    delegationsButton = $("#delegations-button"),
+    voteButton = $("#vote_button"),
+    voteDialog = $("#question-vote-modal");
 
   delegationsButton.click(() => {
     delegationDialog.foundation("open");
@@ -19,6 +20,11 @@ $(() => {
     delegationField.val($(evt.currentTarget).data("delegation-id"));
     delegationCalloutsMessage.text($(evt.currentTarget).data("delegation-granter-name"));
     delegationCallouts.removeClass("is-hidden");
+  });
+
+  delegationUnVoteButtons.click((evt) => {
+    delegationDialog.foundation("close");
+    delegationCallouts.addClass("is-hidden");
   });
 
   voteButton.click(() => {
