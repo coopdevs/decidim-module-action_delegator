504980f5b5fbf13cefb4c65da55ae0c5cff9a0ce
Ivan Vergés <ivan@pokecode.net>
2024-02-14 17:22:40 +0100

    fix ajax delegations voting (#169)
    * fix ajax delegations voting
    
    * linting
    
    * fix chrome
    
    * bump version

--- app/packs/src/decidim/action_delegator/questions.js
+++ app/packs/src/decidim/action_delegator/questions.js
@@ -4,6 +4,7 @@ $(() => {
       delegationCalloutsMessage = $(".delegation-callout-message"),
       delegationDialog = $("#delegations-modal"),
       delegationField = $("#decidim_consultations_delegation_id"),
+      delegationUnVoteButtons = $(".delegation_unvote_button"),
       delegationVoteButtons = $(".delegation-vote-button"),
       delegationsButton = $("#delegations-button"),
       voteButton = $("#vote_button"),
@@ -19,6 +20,11 @@ $(() => {
     delegationField.val($(evt.currentTarget).data("delegation-id"));
     delegationCalloutsMessage.text($(evt.currentTarget).data("delegation-granter-name"));
     delegationCallouts.removeClass("is-hidden");
+  });
+
+  delegationUnVoteButtons.click(() => {
+    delegationDialog.foundation("close");
+    delegationCallouts.addClass("is-hidden");
   });
 
   voteButton.click(() => {
