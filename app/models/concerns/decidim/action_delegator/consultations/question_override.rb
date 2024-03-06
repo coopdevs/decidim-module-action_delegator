a5a2c71b629ca5022d4d4930d7da6cd3cd317ea2
Anna Topalidi <t.opalidisanna@gmail.com>
2024-03-06 09:23:48 +0100

    add weighted result

--- app/models/concerns/decidim/action_delegator/consultations/question_override.rb
+++ app/models/concerns/decidim/action_delegator/consultations/question_override.rb
@@ -11,6 +11,24 @@ module Decidim
           def publishable_results?
             (ActionDelegator.admin_preview_results || consultation.finished?) && sorted_results.any?
           end
+
+          def weighted_responses
+            @weighted_responses ||= Decidim::ActionDelegator::SumOfWeights.new(consultation).query.group_by(&:question_id)
+          end
+
+          def total_weighted_votes
+            @total_weighted_votes ||= weighted_responses[id].sum(&:votes_count)
+          end
+
+          def most_weighted_voted_response
+            weighted_responses[id].max_by(&:votes_count)
+          end
+
+          def responses_sorted_by_weighted_votes
+            @responses_sorted_by_weighted_votes ||= weighted_responses.transform_values do |responses|
+              responses.sort_by { |response| -response.votes_count }
+            end
+          end
         end
       end
     end
